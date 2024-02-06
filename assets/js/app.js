cb87872b4eb88edefdf7afa83fc184b1062438ef
iamrk-js <88290009+iamrk-js@users.noreply.github.com>
2024-02-06 12:38:07 +0530

    qq

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,164 +6,177 @@ const bodyControl = document.getElementById("body");
 const userIdControl = document.getElementById("userId");
 const submitBtn = document.getElementById("submitBtn");
 const updateBtn = document.getElementById("updateBtn");
-let baseUrl = `https://jsonplaceholder.typicode.com`;
-
-let postsUrl = `${baseUrl}/posts`
-
-let postsArray = [];
-
-// XMLHttpRequest >> Constructor function
-// POST  >> CREATE
-// GET  >> READ
-// PUT/PATCH >> UPDATE
-// DELETE  >> DELETE
+const loader = document.getElementById("loader");
 
 
-const onEdit = (ele) => {
-    // cl(ele.closest(".card").id)
-    let getEditId = ele.closest(".card").id;
-    cl(getEditId)
-    localStorage.setItem("getEditId", getEditId);
-    let editUrl = `${baseUrl}/posts/${getEditId}`;
+let baseUrl = `https://batch-10-js-crud-posts-default-rtdb.asia-southeast1.firebasedatabase.app`;
 
-    makeApiCall("GET", editUrl);
+let postUrl = `${baseUrl}/posts.json`
+
+// baseUrl/posts/:id.json
+
+const objToArr = (obj) => {
+    let arr = [];
+    for (const key in obj) {
+        let newObj = obj[key];
+        newObj.id = key;
+        arr.push(newObj)
+    }
+    return arr;
 }
 
+const onEdit = (ele) => {
+    let editId = ele.closest(".card").id;
+    localStorage.setItem("editId", editId);
+    let editUrl = `${baseUrl}/posts/${editId}.json`;
+
+    makeApiCall("GET", editUrl)
+        .then(res => {
+            cl(res)
+            // callback funct....
+            let post = JSON.parse(res);
+            updateBtn.classList.remove("d-none");
+            submitBtn.classList.add("d-none");
+            titleControl.value = post.title;
+            bodyControl.value = post.body;
+            userIdControl.value = post.userId;
+        })
+        .catch(err => {
+            cl(err)
+        })
+}
 
 const onDelete = (ele) => {
     cl(ele)
     let deleteId = ele.closest(".card").id;
-    let deleteUrl = `${baseUrl}/posts/${deleteId}`;
+    let deleteUrl = `${baseUrl}/posts/${deleteId}.json`;
 
-    makeApiCall("DELETE", deleteUrl)
+    let getConfirm = confirm(`Are You sure you want to delete this Post?`)
+
+    if (getConfirm) {
+        makeApiCall("DELETE", deleteUrl)
+            .then(res => {
+                cl(res);
+                let deleteCard = document.getElementById(deleteId);
+                deleteCard.remove();
+            })
+            .catch(cl)
+    }
+
 }
 
-const templatingOfPosts = (arr) => {
-    let result = ``;
-    arr.forEach(post => {
-        result += `
-        <div class="card mb-4" id="${post.id}">
-            <div class="card-header">
-                <h2>${post.title}</h2>
-            </div>
-            <div class="card-body">
-                <p>
-                    ${post.body} 
-                </p>
-            </div>
-            <div class="card-footer d-flex justify-content-between">
-                <button class="btn btn-outline-primary" onclick="onEdit(this)">
-                    Edit
-                </button>
-                <button class="btn btn-outline-danger"
-                onclick="onDelete(this)"
-                >
-                    Delete
-                </button>
-            </div>
-        </div>
-           `
-    });
-    postsContainer.innerHTML = result;
-}
-
-const createCards = (postObj) => {
-    let card = document.createElement('div');
+const createCard = (postObj) => {
+    let card = document.createElement("div");
     card.className = "card mb-4";
     card.id = postObj.id;
     card.innerHTML = `
-                    <div class="card-header">
-                        <h2>${postObj.title}</h2>
-                    </div>
-                    <div class="card-body">
-                        <p>
-                            ${postObj.body} 
-                        </p>
-                    </div>
-                    <div class="card-footer d-flex justify-content-between">
-                        <button class="btn btn-outline-primary" onclick="onEdit(this)">
-                            Edit
-                        </button>
-                        <button class="btn btn-outline-danger"
-                        onclick="onDelete(this)"
-                        >
-                            Delete
-                        </button>
-                    </div>
-                    `;
-    postsContainer.append(card);
-    cl(card)
+                <div class="card-header">
+                    <h2 class="m-0">
+                        ${postObj.title}
+                    </h2>
+                </div>
+                <div class="card-body">
+                    <p class="m-0">
+                    ${postObj.body}
+                    </p>
+                </div>
+                <div class="card-footer d-flex justify-content-between">
+                    <button class="btn btn-primary" type="button" onclick="onEdit(this)">Edit</button>
+                    <button class="btn btn-danger" type="button" onclick="onDelete(this)">Delete</button>
+                </div>
+                `
+
+    postsContainer.append(card)
 }
 
-const makeApiCall = (methodName, apiUrl, bodyMsg = null) => {
-    let xhr = new XMLHttpRequest();
-    // xhr.open("GET", postsUrl);
-    xhr.open(methodName, apiUrl);
-    xhr.send(JSON.stringify(bodyMsg)); //  we send data to DB
-    xhr.onload = function () {
-        if (xhr.status >= 200 || xhr.status <= 299 && xhr.readyState === 4) { // 2xx
-            cl(xhr.response)
-            // templating
-            if (methodName === "GET") {
-                // here data may be Array or it may be Object
-                let data = JSON.parse(xhr.response);
-                if (Array.isArray(data)) {
-                    templatingOfPosts(data);
-                } else {
-                    // we will patch data in form
-                    // cl(data)
-                    updateBtn.classList.remove('d-none');
-                    submitBtn.classList.add('d-none');
-                    titleControl.value = data.title;
-                    bodyControl.value = data.body;
-                    userIdControl.value = data.userId;
-                }
-            }else if(methodName === "PUT"){
-                cl(xhr.response)
-                let id = JSON.parse(xhr.response).id;
-                let card = document.getElementById(id);
-                cl(card)
-                let cardChild = [...card.children]
-                cardChild[0].innerHTML = `<h2>${bodyMsg.title}</h2>`
-                cardChild[1].innerHTML = `<p>${bodyMsg.body}</p>`
-                // cl(cardChild[0])
-                // cl(cardChild[1])
-                postForm.reset();
-                updateBtn.classList.add('d-none');
-                submitBtn.classList.remove('d-none');
-            }else if(methodName === "DELETE"){
-                let getIndex = apiUrl.indexOf('posts/')
-                let id = apiUrl.slice(getIndex + 6);
-                cl(id)
-                document.getElementById(id).remove()
-            }
-        }
-    }}
+const templatingOfPosts = (posts) => {
+    // postsContainer.innerHTML = '';
+    posts.forEach(post => {
+        createCard(post)
+    });
+}
 
-makeApiCall("GET", postsUrl);
+const makeApiCall = (methodName, apiUrl, body = null) => {
+    return new Promise((resolve, reject) => {
+        loader.classList.remove('d-none');
+        let xhr = new XMLHttpRequest();
+        xhr.open(methodName, apiUrl, true);
+        xhr.onload = function () {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                resolve(xhr.responseText);
+                loader.classList.add('d-none');
+            } else {
+                reject(xhr.statusText);
+                loader.classList.add('d-none');
+            }
+
+        }
+        xhr.send(body);
+    })
+}
+
+//SOLID 
+makeApiCall("GET", postUrl)
+    .then(res => {
+        let data = JSON.parse(res);
+        // cl(data)
+        let postArr = objToArr(data);
+        cl(postArr);
+        templatingOfPosts(postArr);
+    })
+    .catch(err => {
+        cl(err)
+    })
 
 const onPostUpdate = () => {
-    let updatedPostObj = {
+    let updateId = localStorage.getItem("editId")
+    let updateUrl = `${baseUrl}/posts/${updateId}.json`;
+    let updatedObj = {
         title: titleControl.value,
         body: bodyControl.value,
         userId: userIdControl.value
     }
-    cl(updatedPostObj)
-    let updateId = localStorage.getItem("getEditId");
-    let updateUrl = `${baseUrl}/posts/${updateId}`;
-    makeApiCall("PUT", updateUrl, updatedPostObj);
+    makeApiCall("PUT", updateUrl, JSON.stringify(updatedObj))
+        .then(res => {
+            cl(res);
+            let data = JSON.parse(res);
+            let getCard = document.getElementById(updateId);
+            cl(getCard)
+            let cardChild = [...getCard.children];
+            cl(cardChild)
+            cardChild[0].innerHTML = `<h2 class="m-0">${data.title}</h2>`;
+            cardChild[1].innerHTML = `<p class="m-0">${data.body}</p>`;
+        })
+        .catch(cl)
+        .finally(() => {
+            postForm.reset();
+            updateBtn.classList.add('d-none');
+            submitBtn.classList.remove('d-none');
+        })
 }
 
-updateBtn.addEventListener("click", onPostUpdate)
+const onPostSubmit = (eve) => {
+    eve.preventDefault();
+    let newPost = {
+        title: titleControl.value,
+        body: bodyControl.value,
+        userId: userIdControl.value
+    }
+    cl(newPost);
 
+    makeApiCall("POST", postUrl, JSON.stringify(newPost))
+        .then(res => {
+            cl(res)
+            let post = newPost;
+            post.id = JSON.parse(res).name
+            createCard(post)
+        })
+        .catch(cl)
+        .finally(() => {
+            // 
+            eve.target.reset()
+        })
+}
 
-
-// makeApiCall("GET", `https://jsonplaceholder.typicode.com/todos`);
-
-// xhr.readyState  0 to 4
-
-// 0 >> XHR Object is created but open method is not called yet
-// 1 >> Open method is called
-// 2 >> send method is called
-// 3 >> server is working on your request
-// 4 >> the API call is completed (It may be success(data) or it may be fail(error))
+updateBtn.addEventListener("click", onPostUpdate);
+postForm.addEventListener("submit", onPostSubmit)
